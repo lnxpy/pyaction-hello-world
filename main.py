bc3aa2494fe79c207c8989b3ceb1ce7911da708b
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2024-04-13 20:19:09 +0330

    Initial commit

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import sys
 from typing import List
 
-from pyaction.auth import Auth
-from pyaction.issues import IssueForm
-
 from pyaction import io
-
-import logging
 
 
 def main(args: List[str]) -> None:
@@ -16,17 +11,7 @@ def main(args: List[str]) -> None:
         args: STDIN arguments
     """
 
-    token = io.read("github_token")
-    auth = Auth(token=token)
-    auth.authenticate()
-
-    repo = auth.github.get_repo(io.read("repository"))
-    user_input = IssueForm(repo=repo, number=int(io.read("issue_number"))).render()
-
-    logging.error(token, "pyaction")
-    logging.error(f"{token} | {len(token)}", "pyaction")
-    logging.error(repo, "pyaction")
-    logging.error(user_input, "pyaction")
+    pass
 
 
 if __name__ == "__main__":
