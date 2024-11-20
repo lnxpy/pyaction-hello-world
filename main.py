103fc4eb241024d3f4aaa3f2099a33b770aa1427
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2024-11-20 16:45:55 +0330

    updated

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 from pyaction import PyAction
+from pyaction.workflow.stream import WorkflowContext
+
+workflow = PyAction()
+
+
+@workflow.action
+def my_action(name: str) -> None:
+    context = WorkflowContext(
+        {
+            "phrase": f"Hi {name}!",
+        }
+    )
+    workflow.write(context)
